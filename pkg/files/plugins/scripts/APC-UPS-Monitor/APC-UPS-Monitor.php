98f00b1be0d4f5a8702a12764801dd9ec58d860c
Robert Vandervoort <bigrobintx@gmail.com>
2016-08-01 12:56:45 -0500

    initial push
    v1.0 of my APC UPS monitor plugin

---
+++ pkg/files/plugins/scripts/APC-UPS-Monitor/APC-UPS-Monitor.php
@@ -0,0 +1,78 @@
+<?
+//Ensure SNMP extention is loaded and if not FAIL
+if (!extension_loaded("snmp")) {
+	echo "PHP SNMP Extension not loaded!";
+	exit(2);
+}
+$SNMP_Host = getenv('UPTIME_HOSTNAME');
+$SNMP_Port = getenv('UPTIME_SNMP-PORT');
+$SNMP_Community = getenv('UPTIME_READ-COMMUNITY');
+$SNMP_Connection_String = $SNMP_Host . ":" . $SNMP_Port;
+
+//Check to see if we can connect and if it is an APC or not and IF not, fail.
+$sysDescrQuery = snmpget("$SNMP_Connection_String", "$SNMP_Community", "sysDescr.0", 1000000); 
+
+if ($sysDescrQuery === false) {
+	echo "Cannot read sysDescr.0 data from host.";
+	exit(2);
+} elseif (strpos($sysDescrQuery, 'APC') !== false) {
+	} else {
+		echo $sysDescrQuery;
+		echo "Device description indicicates it is not an APC branded device, exiting.";
+		exit(2);
+	}
+
+// Everything must be good to this point so pull the data from the UPS!
+$stateQuery = snmpget("$SNMP_Connection_String", "$SNMP_Community", ".1.3.6.1.4.1.318.1.1.1.4.1.1.0", 1000000); 
+$state = str_replace(array('INTEGER: ', '"'),"", $stateQuery);
+
+if ($stateQuery) {
+	$chargeQuery = @snmpget("$SNMP_Connection_String", "$SNMP_Community", ".1.3.6.1.4.1.318.1.1.1.2.2.1.0", 1000000); 
+	$charge = str_replace(array('Gauge32: ', '"'),"",$chargeQuery);
+	if ($state == 1) {
+		$output = "State unknown"; 
+		echo "state_int: ".$state."\n";
+		echo "state: ".$output."\n";
+		echo "charge: ".$charge;
+		exit(1); 
+	} 
+	if ($state == 2) {
+		$output = "Power connected";
+		echo "state_int: ".$state."\n";
+		echo "state: ".$output."\n";
+		echo "charge: ".$charge;
+		exit(0);
+	} 
+	if ($state == 3) {
+		$output = "Power lost. Running on batteries. $charge% left."; 
+		echo "state_int: ".$state."\n";
+		echo "state: ".$output."\n";
+		echo "charge: ".$charge;
+		exit(1); 
+	} 
+	if ($state == 7) {
+		$output = "UPS turned off";
+		echo "state_int: ".$state."\n";
+		echo "state: ".$output."\n";
+		echo "charge: ".$charge;
+		exit(1);
+	}
+	if ($state == 8) {
+		$output = "UPS is rebooting";
+		echo "state_int: ".$state."\n";
+		echo "state:  ".$output."\n";
+		echo "charge:  ".$charge;
+		exit(1);
+	}
+	if ($state == 11) {
+		$output = "UPS is sleeping until power returns";
+		echo "state_int: ".$state."\n";
+		echo "state: ".$output."\n";
+		echo "charge: ".$charge;
+		exit(1);
+	}
+} else {
+	echo "Cannot read data from host.";
+	exit(2);
+} 
+?> 
---
+++ pkg/files/scripts/APC-UPS-Monitor/APC-UPS-Monitor.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+set PHPDIR=..\..\..\apache\php\
+"%PHPDIR%\php.exe" APC-UPS-Monitor.php
